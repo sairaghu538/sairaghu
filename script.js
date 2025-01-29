f161ff0536b99526ed32583341a36211d7d85d99
sairaghu538 <101308958+sairaghu538@users.noreply.github.com>
2025-01-29 18:21:10 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+<script src="script.js"></script>
+
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.querySelector('#myButton');
+    button.addEventListener('click', function() {
+        alert('Button clicked!');
+    });
+});
